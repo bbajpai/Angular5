5755f3472f0273f429a22ea6b29d6746a3f7fd73
Aakash Handa <aakash.h@hcl.com>
2017-10-09 20:59:08 +0530

    A

---
+++ app/app.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+
+@Component({
+	selector: 'pm-app',
+	template: `<div>
+		<h1>This is ang2</h1>
+		<pm-product></pm-product>
+	</div>`
+})
+
+export class AppComponent{
+
+}
